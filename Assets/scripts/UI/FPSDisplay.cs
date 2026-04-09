5d8cdb6f0cc32f9d606c873b8bf50a78acfe33e8
HunHunlA <nguyenminhhienmx@gmail.com>
2026-04-09 19:46:51 +0700

    update FPS

--- Assets/scripts/UI/FPSDisplay.cs
+++ Assets/scripts/UI/FPSDisplay.cs
@@ -1,24 +1,87 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI FpsText;
+    const string DefaultLabel = "-- FPS";
+    static readonly Vector2 TextOffset = new(-24f, -24f);
+    static readonly Vector2 TextSize = new(180f, 40f);
 
-    private float pollingTime = 1f;
-    private float time;
-    private int frameCount;
-    
+    [SerializeField] public TextMeshProUGUI FpsText;
+    [SerializeField, Min(0.1f)] float pollingTime = 0.5f;
+
+    static FPSDisplay instance;
+
+    float elapsedTime;
+    int frameCount;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        UpdateText(0);
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Update()
     {
-        time += Time.deltaTime; 
+        EnsureTextExists();
+
+        elapsedTime += Time.unscaledDeltaTime;
         frameCount++;
-        if (time > pollingTime)
+
+        if (elapsedTime < pollingTime)
         {
-            int frameRate = Mathf.RoundToInt(frameCount / time);
-            FpsText.text = frameRate.ToString() + " FPS";
-            time -= pollingTime;
-            frameCount = 0;
+            return;
+        }
+
+        int frameRate = Mathf.RoundToInt(frameCount / elapsedTime);
+        UpdateText(frameRate);
+
+        elapsedTime = 0f;
+        frameCount = 0;
+    }
+
+    void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
         }
     }
+
+    void EnsureTextExists()
+    {
+    }
+
+    void ConfigureText()
+    {
+    }
+
+    void UpdateText(int frameRate)
+    {
+        if (FpsText == null)
+        {
+            return;
+        }
+
+        if (frameRate > 0)
+        {
+            FpsText.SetText("{0} FPS", frameRate);
+            return;
+        }
+
+        FpsText.text = DefaultLabel;
+    }
+
+
 }
